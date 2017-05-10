1cbb19f61398473953c79b61a06f420b1b2e22a3
hedu <eperezghedu@gmail.com>
2017-05-10 23:42:06 +0200

    end configuration class

--- src/main/java/com/eduardo/idealista/settings/Configuration.java
+++ src/main/java/com/eduardo/idealista/settings/Configuration.java
@@ -13,22 +13,34 @@ public class Configuration {
 
     final static String FILE_PATH = "settings.properties";
 
-    final static String MAIL_RECEIVERS = "mail.receivers";
-    final static String MAIL_FROM = "mail.from";
-    final static String MAIL_PASSWORD = "mail.password";
-    final static String FILTER_ZONES = "filter.zones";
-    final static String FILTER_MAX_PRICE = "filter.max.price";
-    final static String FILTER_MIN_ROOMS = "filter.min.rooms";
-    final static String FILTER_INCLUDE_GROUND_FLOOR = "filter.include.ground.floor";
-    final static String FILTER_PICTURES_REQUIRED = "filter.pictures.required";
-    final static String FILTER_PUBLISHED_PERIOD = "filter.published.period";
+    public final static String MAIL_RECEIVERS = "mail.receivers";
+    public final static String MAIL_FROM = "mail.from";
+    public final static String MAIL_PASSWORD = "mail.password";
+    public final static String FILTER_ZONES = "filter.zones";
+    public final static String FILTER_MAX_PRICE = "filter.max.price";
+    public final static String FILTER_MIN_ROOMS = "filter.min.rooms";
+    public final static String FILTER_INCLUDE_GROUND_FLOOR = "filter.include.ground.floor";
+    public final static String FILTER_PICTURES_REQUIRED = "filter.pictures.required";
+    public final static String FILTER_PUBLISHED_PERIOD = "filter.published.period";
 
     private Properties props;
 
     public Configuration() {
 
         props = new Properties();
+        //setDefaultValues();
 
+        try {
+            // load a properties file
+            props.load(getClass().getClassLoader().getResourceAsStream("settings.properties"));
+
+        } catch (IOException ex) {
+            ex.printStackTrace(System.out);
+        }
+        System.out.println(props.toString());
+    }
+
+    private void setDefaultValues() {
         //set default values
         Map<String, List<String>> zones = new HashMap<>();
 
@@ -47,28 +59,39 @@ public class Configuration {
         props.put(FILTER_INCLUDE_GROUND_FLOOR, "false");
         props.put(FILTER_PICTURES_REQUIRED, "true");
         props.put(FILTER_PUBLISHED_PERIOD, "day");
-
-        try {
-            // load a properties file
-            props.load(getClass().getClassLoader().getResourceAsStream("settings.properties"));
-
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-        System.out.println(props.toString());
     }
 
+    public Map<String, List<String>> getMap(String key) {
+        Map<String, List<String>> result = new HashMap<>();
+        String[] array = props.getProperty(key).split(";");
+        Arrays.stream(array).forEach(str -> {
+            String[] list = str.split("=");
+            result.put(list[0], toList(list[1]));
+        });
 
-    private List<String> parseList(String list) {
-        List<String> result = new ArrayList<>();
-        String[] array = list.split(",");
-        for (String str: array) {
-            result.add(str);
-        }
         return result;
     }
 
-    private int getInt(String key) {
+    public String get(String key) {
+        return props.getProperty(key);
+    }
+
+    public List<String> getList(String key) {
+        return toList(props.getProperty(key));
+    }
+
+    private List<String> toList(String value) {
+        List<String> result = new ArrayList<>();
+        String[] array = value.split(",");
+        Arrays.stream(array).forEach(str -> result.add(str));
+        return result;
+    }
+
+    public boolean getBoolean(String key) {
+        return Boolean.valueOf(key);
+    }
+
+    public int getInt(String key) {
         int result = 0;
         if (props.containsKey(key)) {
             String value = props.getProperty(key);
